d56dea9990423cb05fda78106f19d0ee30815208
mhendricks96 <mhendricks96@yahoo.com>
2021-06-27 20:36:44 -0700

    touched up

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -11,8 +11,6 @@ class Game:
   dice_left = 6
 
   def __init__(self):
-    #self.dice_left = 6
-    #self.score = GameLogic.score
     self.banker = Banker()
 
   #def dice_kept(self):
@@ -38,7 +36,6 @@ class Game:
     print("Welcome to Game of Greed")
     print("(y)es to play or (n)o to decline")
     user_start = input("> ")
-    #print(user_start)
     if user_start == "y":
       self.start_game()
     elif user_start == "n":
@@ -46,50 +43,43 @@ class Game:
       quit()
     else:
       print("just answer the question")
-    #print("Enter dice to keep, or (q)uit:")
-    #self.repetitive_gameplay(user_start)
+    
 
     
   def repetitive_gameplay(self, saved_dice):
     self.saved_dice = saved_dice
     dice_list = [int(x) for x in str(saved_dice)]
-    
-    #saved_dice = self.get_saved_dice(keep_playing)
     self.shelved += GameLogic.calculate_score(dice_list)
     self.dice_left = 6 - len(saved_dice)
 
-
     self.ask_to_play_again()
-    keep_playing = input("> ")
-    
+    keep_playing = input("> ")    
     
     while keep_playing != "q":
-      
       if keep_playing == "r":
         self.roller(6)
       elif keep_playing == "b":
         shelved_this_round = self.shelved
         Banker.bank(self)
-        #go to next loop
       else:
         return "thats not an option"
         keep_playing = input("> ")
-      # this should be now balance - old balance
-      #print(f"{shelved_this_round}")
+
       if self.round_counter < 2:
         self.give_round_score(shelved_this_round)
+      
       self.start_game()
-      #print("Enter dice to keep, or (q)uit:")
       dice_kept = input("> ")
       if dice_kept == "q":
         print(f"Thanks for playing. You earned {self.balance} points")
         quit()
       else:
         dice_kept_list = [int(x) for x in str(dice_kept)]
-        #print(f"{dice_kept_list}")
+
         self.roller(len(dice_kept_list))
         self.shelved += GameLogic.calculate_score(dice_kept_list)
         self.dice_left = 6 - len(dice_kept_list)
+        
         self.ask_to_play_again()
         answer = input("> ")
         if answer == "r":
@@ -98,7 +88,6 @@ class Game:
           shelved_this_round = self.shelved
           Banker.bank(self)
           self.give_round_score(shelved_this_round)
-          
         elif answer == "q":
           print(f"Thanks for playing. You earned {self.balance} points")
           quit()
@@ -114,15 +103,13 @@ class Game:
     self.round_counter +=1
     self.dice_left = 6
     roll = "665421"#this is fake for the test. use the one below this
-    ##roll = roller(self.dice_left)
+    #roll = roller(self.dice_left)#REAL dice roll!!
     self.dice = dice
     print(f"Starting round {self.round_counter}")
     print(f"Rolling {self.dice_left} dice...")
-          
     string_dice = ""
     for dice in roll:
       string_dice += f"{str(dice)} "
-
     print(f"*** {string_dice}***")
 
 
@@ -143,22 +130,18 @@ class Game:
     string_dice = ""
     for dice in roll:
       string_dice += f"{str(dice)} "
-
     print(f"Rolling {self.dice_left} dice...")
     print(f"*** {string_dice}***")
-    #print("why does this pass?")
     #return roll
 
   
   def start_game(self):
-    #self.round_counter += 1
     
     dice_left = 6
     #if dice_left == 0:
       #pass #exit game or something
     # roll = tuple(random.randint(1, 6) for i in range(dice_left))
     self.fake_dice_roll()
-    #print(f"{self.round_counter}")
     if self.round_counter < 2:
       print("Enter dice to keep, or (q)uit:")
       quit_or_play_again = input("> ")
@@ -166,13 +149,10 @@ class Game:
         print(f"Thanks for playing. You earned {self.balance} points")
         quit()
       else:
-        #self.ask_to_play_again()
         self.repetitive_gameplay(quit_or_play_again)
-  
     else:
       print("Enter dice to keep, or (q)uit:")
-      #quit_or_play_again = input("> ")
-      #self.repetitive_gameplay(quit_or_play_again)
+      
 
 
 
